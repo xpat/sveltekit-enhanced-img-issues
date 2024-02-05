40736b71b8c76a1a6dc96415cf331001cb628146
ersoc <122680687+ersoc@users.noreply.github.com>
2024-02-05 21:50:26 +0100

    feat: add examples showcasing issues with enhanced:img component

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import svelteLogo from '../images/svelte-logo-square.png?enhanced';
+    const fileName = "svelte-logo-square";
+    const fileExtension = "png";
+    const pathToFileTemplate = `../images/${fileName}.${fileExtension}`;
+    const pathToFile = '../images/svelte-logo-square.png';
+</script>
+
+<div>
+    <!-- these work fine -->
+    <enhanced:img src={svelteLogo} alt="" />
+    <enhanced:img src='../images/svelte-logo-square.png' alt="" />
+    
+    <!-- these don't work -->
+    <enhanced:img src={'../images/svelte-logo-square.png'} alt="" />
+    <enhanced:img src={`../images/${fileName}.${fileExtension}`} alt="" />
+    <enhanced:img src={pathToFileTemplate} alt="" />
+    <enhanced:img src={pathToFile} alt="" />
+</div>
