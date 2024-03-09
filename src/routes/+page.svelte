0bcee8b665f2925c66ab0897ed416299d32fe46f
Christophe R Patraldo <cheftech@patraldo.dev>
2024-03-09 13:26:15 -0600

    Update +page.svelte PNG, GIF & MP4
    PNG works, GIFS partially work (image appears but doesn't move), and MP4 results in a 500 server error.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,39 @@
 <script lang="ts">
+    import myPNG from '../images/pinkmonsterteethy.png?enhanced';
+    const fileName = "pinkmonsterteethy";
+    const fileExtension = "png";
+    const pathToFileTemplate = `../images/${fileName}.${fileExtension}`;
+    const pathToFile = '../images/pinkmonsterteethy';
+</script>
+
+<div>
+     <!-- these work fine -->
+     <enhanced:img src={myPNG} alt='' />
+     <enhanced:img src='../images/pinkmonsterteethy.png' alt="" />
+</div>
+
+<!--- Without Typescript, const and div -->
+<script>
+     import myPNG from '../images/pinkmonsterteethy.png?enhanced';
+     import myVideo from '../images/video1.mp4?enhanced';
+     import myGIF from '../images/image.gif?enhanced';
+</script>
+
+
+     <!-- these work fine -->
+     <enhanced:img src={myPNG} alt='' />
+     <enhanced:img src='../images/pinkmonsterteethy.png' alt="" />
+
+     <!-- these partially work -GIFs appear using either the variable or the path -->
+     <enhanced:img src={myGIF} alt='' />
+     <enhanced:img src='../images/image.gif' alt="" />
+         
+     <!-- these don't work -->
+     <enhanced:img src={myVideo} alt='' />
+     <enhanced:img src='../images/video1.mp4' alt="" />
+
+
+<script lang="ts">
     import svelteLogo from '../images/svelte-logo-square.png?enhanced';
     const fileName = "svelte-logo-square";
     const fileExtension = "png";
